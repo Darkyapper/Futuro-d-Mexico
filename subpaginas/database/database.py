7fd3a9bc7682c5d61235bccd475b7f460927964b
DavidSan1108 <152447414+DavidSan1108@users.noreply.github.com>
2024-03-05 09:56:15 -0600

    database-fer

--- subpaginas/database/database.py
+++ subpaginas/database/database.py
@@ -1,8 +1,9 @@
 import mysql.connector
 
 database = mysql.connector.connect(
-    host='localhost:3306',
+    host='localhost',
+    port='3306'
     user='id21959562_user1',
     password='David17092002@',
-    database='id21959562_python_bbd'
+    database='id21959562_maquina'
 )
