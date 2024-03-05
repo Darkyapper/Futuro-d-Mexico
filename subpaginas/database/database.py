3439c7078a0dd78e709859c1a35f993340d63dfd
David Chay <yapperdark2@gmail.com>
2024-03-05 09:56:58 -0600

    Chay - correción minima

--- subpaginas/database/database.py
+++ subpaginas/database/database.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 database = mysql.connector.connect(
     host='localhost',
-    port='3306'
+    port='3306',
     user='id21959562_user1',
     password='David17092002@',
     database='id21959562_maquina'
