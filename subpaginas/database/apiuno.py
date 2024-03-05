a2fa7feaad782ede9e7b4dc74634c67ca14395a4
David Chay <yapperdark2@gmail.com>
2024-03-05 16:44:22 -0600

    Chay - Actualización de la base de datos y api

--- subpaginas/database/apiuno.py
+++ subpaginas/database/apiuno.py
@@ -6,6 +6,17 @@ conn = sqlite3.connect('baseuno.sqlite')
 # Crear un cursor
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM machines")
+#SELECCIONAR
+#cursor.execute("SELECT * FROM machines")
+
+#INSERTAR
+#cursor.execute("INSERT INTO machines (serial_no,status,location) VALUES (97251,'On','Tixkokob')")
+#conn.commit()
+
+#BORRAR
+#cursor.execute("DELETE FROM machines WHERE serial_no=97251")
+#conn.commit()
 
 print(cursor.fetchall())
+
+conn.close()
