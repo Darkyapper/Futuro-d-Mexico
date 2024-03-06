205dc8c071f5b393a20df4fafec6747a05bc1848
David Chay <yapperdark2@gmail.com>
2024-03-05 19:28:04 -0600

    Chay - Visualizar y borrar maquinas

--- subpaginas/database/apiuno.py
+++ subpaginas/database/apiuno.py
@@ -3,7 +3,7 @@ import sqlite3, os, time
 conn = sqlite3.connect('baseuno.sqlite')
 cursor = conn.cursor()
 
-def InsertMachine(SN, ST, LOC):
+def InsertMachine(SN, ST, LOC): #Insertar maquinas a la base de datos
     cursor.execute(f"SELECT * FROM machines WHERE serial_no = '{SN}'")
     existing_machine = cursor.fetchone()
     
@@ -18,7 +18,7 @@ def InsertMachine(SN, ST, LOC):
         print("Maquina registrada exitosamente.")
         time.sleep(2)
 
-def DeleteMachine(SN):
+def DeleteMachine(SN): #Borrar maquina de la base de datos
     cursor.execute(f"SELECT * FROM machines WHERE serial_no = '{SN}'")
     existing_machine = cursor.fetchone()
 
@@ -31,8 +31,8 @@ def DeleteMachine(SN):
         print("La maquina no existe.")
         time.sleep(2)
         return
-    
-def EstadoMaquinaUno(SN):
+
+def EstadoMaquinaUno(SN): #Ver el estado de una maquina de la base de datos
     cursor.execute(f"SELECT * FROM machines WHERE serial_no = '{SN}'")
     resultado = cursor.fetchone()
 
@@ -43,6 +43,18 @@ def EstadoMaquinaUno(SN):
         print("Maquina no encontrada.")
         time.sleep(2)
 
+def EstadoMaquinasTodas(): #Ver el estado de todas las maquinas de la base de datos
+    cursor.execute(f"SELECT serial_no, status FROM machines")
+    resultado = cursor.fetchall()
+    if resultado:
+        for serial_no, status in resultado:
+            print(f"No machine:{serial_no} Status:{status}")
+            time.sleep(1)
+    else:
+        print("Maquinas no encontradas.")
+        time.sleep(2)
+
+#MAIN - Ejecutor de los comandos principales
 e = False
 while e == False:
     os.system("cls")
@@ -50,6 +62,7 @@ while e == False:
     print("1.- Insertar una maquina.")
     print("2.- Eliminar una maquina.")
     print("3.- Ver estado de una maquina.")
+    print("4.- Ver estado de todas las maquinas.")
     print("0.- Salir.")
 
     choose = input()
@@ -81,6 +94,8 @@ while e == False:
         print("Escribe el Numero de serie de la maquina:")
         serialnum = int(input())
         EstadoMaquinaUno(serialnum)
+    elif choose == "4":
+        EstadoMaquinasTodas()
     elif choose == "0":
         break
     else:
