38e61a83ff16791f707c8ec7b12b2fc6d400147e
David Chay <yapperdark2@gmail.com>
2024-03-05 21:48:04 -0600

    Chay - Reportes visualizacón apiuno.py

--- subpaginas/database/apiuno.py
+++ subpaginas/database/apiuno.py
@@ -42,27 +42,59 @@ def EstadoMaquinaUno(SN): #Ver el estado de una maquina de la base de datos
     else:
         print("Maquina no encontrada.")
         time.sleep(2)
+    input()
 
 def EstadoMaquinasTodas(): #Ver el estado de todas las maquinas de la base de datos
     cursor.execute(f"SELECT serial_no, status FROM machines")
     resultado = cursor.fetchall()
     if resultado:
         for serial_no, status in resultado:
-            print(f"No machine:{serial_no} Status:{status}")
+            print(f"No machine: {serial_no} ||Status: {status}")
             time.sleep(1)
     else:
         print("Maquinas no encontradas.")
         time.sleep(2)
+    input()
+
+def VerReportesTodos(): #Ver todos los reportes
+    cursor.execute(f"SELECT no_reporte, detalle, serial_no FROM reportes")
+    resultado = cursor.fetchall()
+
+    if resultado:
+        for no_reporte, detalle, serial_no in resultado:
+            print(f"No Reporte: {no_reporte} ||Descripción: {detalle} ||No Maquina: {serial_no}")
+            time.sleep(1)
+    else:
+        print("No se han encontrado reportes.")
+        time.sleep(2)
+    input()
+
+def VerReporteUno(RN): #Ver un reporte por codigo
+    cursor.execute(f"SELECT * FROM reportes WHERE no_reporte = '{RN}'")
+    resultado = cursor.fetchone()
+
+    if resultado:
+        print("Status: " + resultado[1])
+        time.sleep(2)
+    else:
+        print("No se han encontrado reportes.")
+        time.sleep(2)
+    input()
 
 #MAIN - Ejecutor de los comandos principales
 e = False
 while e == False:
     os.system("cls")
     print("Escoge una opcion para realizar:")
+    print("--------CODIGOS MAQUINAS--------")
     print("1.- Insertar una maquina.")
     print("2.- Eliminar una maquina.")
     print("3.- Ver estado de una maquina.")
     print("4.- Ver estado de todas las maquinas.")
+    print("---------CODIGOS VENTAS---------")
+    print("5.- Ver todos los reportes.")
+    print("6.- Ver un reporte.")
+
     print("0.- Salir.")
 
     choose = input()
@@ -80,6 +112,7 @@ while e == False:
         location = input()
 
         InsertMachine(serialnum, status, location)
+
     elif choose == "2":
         os.system("cls")
         serialnum = ""
@@ -87,6 +120,7 @@ while e == False:
         print("Escribe el Numero de serie de la maquina:")
         serialnum = int(input())
         DeleteMachine(serialnum)
+
     elif choose  == "3":
         os.system("cls")
         serialnum = ""
@@ -94,10 +128,24 @@ while e == False:
         print("Escribe el Numero de serie de la maquina:")
         serialnum = int(input())
         EstadoMaquinaUno(serialnum)
+
     elif choose == "4":
         EstadoMaquinasTodas()
+
+    elif choose == "5":
+        VerReportesTodos()
+
+    elif choose == "6":
+        os.system("cls")
+        serialnum = ""
+
+        print("Escribe el Numero del reporte:")
+        repornum = int(input())
+        VerReporteUno(repornum)
+
     elif choose == "0":
         break
+
     else:
         os.system("cls")
         print("Esta no es una opción valida.")
