f8987564d7990ecd06b3c86c51059b15bb578c77
David Chay <yapperdark2@gmail.com>
2024-03-05 18:04:34 -0600

    Chay - Modificación apiuno.py y baseuno.sqlite

--- subpaginas/database/apiuno.py
+++ subpaginas/database/apiuno.py
@@ -1,22 +1,50 @@
-import sqlite3
+import sqlite3, os, time
 
-# Conectar a la base de datos
 conn = sqlite3.connect('baseuno.sqlite')
-
-# Crear un cursor
 cursor = conn.cursor()
 
-#SELECCIONAR
-#cursor.execute("SELECT * FROM machines")
+def InsertMachine(SN, ST, LOC):
+    cursor.execute(f"SELECT * FROM machines WHERE serial_no = '{SN}'")
+    existing_machine = cursor.fetchone()
+    
+    if existing_machine:
+        print("Ya existe una máquina con este número de serie en la base de datos.")
+        time.sleep(2)
+        return
+    else:
+        cursor.execute(f"INSERT INTO machines (serial_no,status,location) VALUES ('{SN}','{ST}','{LOC}')")
+        conn.commit()
+        conn.close()
+        print("Maquina registrada exitosamente.")
+        time.sleep(2)
 
-#INSERTAR
-#cursor.execute("INSERT INTO machines (serial_no,status,location) VALUES (97251,'On','Tixkokob')")
-#conn.commit()
+e = False
 
-#BORRAR
-#cursor.execute("DELETE FROM machines WHERE serial_no=97251")
-#conn.commit()
+while e == False:
+    os.system("cls")
+    print("Escoge una opcion para realizar:")
+    print("1.- Insertar una maquina.")
+    print("2.- Eliminar una maquina.")
+    print("0.- Salir.")
 
-print(cursor.fetchall())
+    choose = input()
+    if choose == "1":
+        os.system("cls")
+        serialnum = ""
+        status = ""
+        location = ""
 
-conn.close()
+        print("Escribe el Numero de serie de la maquina:")
+        serialnum = int(input())
+        print("Escribe el estado actual de la maquina:")
+        status = input()
+        print("Escribe la ubicación de la maquina:")
+        location = input()
+
+        InsertMachine(serialnum, status, location)
+
+    elif choose == "0":
+        break
+    else:
+        os.system("cls")
+        print("Esta no es una opción valida.")
