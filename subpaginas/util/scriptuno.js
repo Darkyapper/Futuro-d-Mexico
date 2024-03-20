deb63a6120b75cac89b4d3aedd3db1a064328684
David Chay <yapperdark2@gmail.com>
2024-03-20 14:48:11 -0600

    Chay - Reactivación del texto con contador del JS de registro.html

--- subpaginas/util/scriptuno.js
+++ subpaginas/util/scriptuno.js
@@ -2,7 +2,7 @@ document.getElementById('registrationForm').addEventListener('submit', function(
     event.preventDefault(); // Prevenir el envío del formulario
     var registrationMessage = document.getElementById('registrationMessage');
     registrationMessage.style.display = 'block'; // Mostrar el mensaje de registro
-    /*setTimeout(function() {
+    setTimeout(function() {
         registrationMessage.style.display = 'none'; // Ocultar el mensaje después de 3 segundos
-    }, 3000);*/
+    }, 3000);
 });
